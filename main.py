a492868d56002b22126021bec5d5563653d68533
hryvnia <105789022+Sanagocat@users.noreply.github.com>
2022-05-18 12:25:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+
+# Using for, add from 1 ~ 10 and get sum result!
+sum = 0
+for i in range (1,11): 
+  sum = sum + i
+
+print("My Answer is " + str(sum))
+ultrasum =55
+print("The added anweser is " + str(ultrasum))
+
+
+# Using for, add from 1 3 5 7 9, get odd number sum    
+suum=0
+for i in range (1,10,2):
+  print(i)
+  suum=suum + i
+print("the sum is " + str(suum))
+
+
+# Using for, add from 2 4 6 8 10 12 14 16 18, get even number sum   
+evensum=0
+for i in range (2,19,2):
+  print(i)
+  evensum=evensum+i
+print("the sum is " + str(evensum))
+
+
+# Using for, add from 2 4 6 8 10 12 14 ~~~~ 18000, get even number sum   
+bigevensum=0
+for i in range (2,18001,2):
+  print(i)
+  bigevensum=bigevensum+i
+print("the sum is " + str(bigevensum))
+
+
+
+
+
+
+
